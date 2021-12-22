d8055669f3f7d6ffa795a4f1943008d265bc6307
Mike Burr <mb@unintuitive.org>
2021-12-22 14:25:31 -0700

    .

--- publish.sh
+++ publish.sh
@@ -5,5 +5,6 @@ cd $(dirname "$0")
 git commit -am .
 git push
 emacs README.org --batch -f org-html-export-to-html --kill
+mkdir -p build
 mv -f README.html build/
 rsync -xva ./res ./build/README.html pu:/var/www/unintuitive.org/HoEaHCC/
