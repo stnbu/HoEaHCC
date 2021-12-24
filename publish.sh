bf75870a8b28b8ce458a73af804ae53f076dfed7
Mike Burr <mb@unintuitive.org>
2021-12-24 09:22:14 -0700

    .

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$0")
 
 emacs README.org --batch -f org-html-export-to-html --kill
-rsync -xva ./res ./README.html pu:/var/www/unintuitive.org/HoEaHCC/
+rsync -xa ./res ./README.html pu:/var/www/unintuitive.org/HoEaHCC/
 echo "\n\n\nRefresh your browsers!\n\n\n"
 mkdir -p build
 mv -f ./README.html build/
